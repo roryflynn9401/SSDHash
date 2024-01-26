aa179b498b41a6db8182f747f27d98f96fc5c24d
40259039 <rflynn12@qub.ac.uk>
2024-01-26 14:49:46 +0000

    Remove unused extensions

--- Extensions.cs
+++ Extensions.cs
@@ -7,31 +7,7 @@ using System.Threading.Tasks;
 
 namespace SSDHash
 {
-    internal static class Extensions
+    public static class Extensions
     {
-        internal static bool IsValueTypeOrString(this Type type)
-        {
-            return type.IsValueType || type == typeof(string);
-        }
-
-        internal static string ToStringValueType(this object value)
-        {
-            return value switch
-            {
-                DateTime dateTime => dateTime.ToString("o"),
-                bool boolean => boolean.ToStringLowerCase(),
-                _ => value.ToString()
-            };
-        }
-
-        internal static bool IsIEnumerable(this Type type)
-        {
-            return type.IsAssignableTo(typeof(IEnumerable));
-        }
-
-        internal static string ToStringLowerCase(this bool boolean)
-        {
-            return boolean ? "true" : "false";
-        }
     }
 }
