c2a5dbf65268b1a4bbb9e8cff77805d3d833702d
40259039 <rflynn12@qub.ac.uk>
2024-03-05 21:40:42 +0000

    Readd Dissimilarity Calculation function, more restructuring

--- LogAnalyser/Program.cs
+++ LogAnalyser/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+                switch (arg)
+                {
+                    case "--help":
+                        HelpMenu();
+                        break;
+                    //TODO Usage Paramaters
+                }
+            }
         }
+
+        #region Menu Prints
+
+        private static void HelpMenu()
+        {
+            Console.WriteLine($"""
+            This is the help menu for SSDHash. Below are the command-line arguments available:
+            -h|--help : Help Manu,
+
+
+            """);
+        }
+
+        #endregion
     }
 }
